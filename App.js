78f521bba38c1876c506e800a5b9082b7b2b3355
a0ki-github <aoki.hotaka@gmail.com>
2022-07-30 18:22:31 +0900

    add: use State

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Hello World!</Text>
       <Greeting name="Bob" />
       <Greeting name="Mike" />
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Add Count."
+      />
+      <Text>Current count is {count}</Text>
     </View>
   );
 }
