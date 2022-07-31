bd11c824283e80e9f001e5a7374bdd1354bc13fa
a0ki-github <aoki.hotaka@gmail.com>
2022-07-31 09:17:26 +0900

    add: use ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
@@ -8,20 +8,22 @@ export default function App() {
   const numbers = [...Array(100)].map((_, i) => "hoge" + i)
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Hello World!</Text>
-      <Greeting name="Bob" />
-      <Greeting name="Mike" />
-      <Button
-        onPress={() => setCount(count + 1)}
-        title="Add Count."
-      />
-      <Text>Current count is {count}</Text>
-      {numbers.map((number) => (
-        <Text>{number}</Text>
-      ))}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Hello World!</Text>
+        <Greeting name="Bob" />
+        <Greeting name="Mike" />
+        <Button
+          onPress={() => setCount(count + 1)}
+          title="Add Count."
+        />
+        <Text>Current count is {count}</Text>
+        {numbers.map((number) => (
+          <Text>{number}</Text>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
